b336cf78235da6a6cdecc17461e905778024c69c
n0tx <rcandra91@msn.com>
2023-12-21 23:55:13 +0700

    Initialize commit

---
+++ customer-restclient/src/test/java/com/riki/customer/CustomerRestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRestclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
